22873dc84b31bba04f8957f8be2d037a5fd04d5d
MarcoCitterio <MARCO.CITTERIO1@issgreppi.it>
2023-03-24 15:19:52 +0100

    Inizio progetto - aggiunta nav controller, bottom menu e fragment

--- app/src/main/java/com/example/eventiapp/MainActivity.java
+++ app/src/main/java/com/example/eventiapp/MainActivity.java
@@ -2,7 +2,17 @@ package com.example.eventiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+
+
     }
 }
